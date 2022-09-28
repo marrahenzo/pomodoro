31759da20e744c58e03e630f7970eda2a1242e28
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-09-28 15:18:36 -0300

    Refactor millisecondsToMinutesAndSeconds function

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,9 +1,8 @@
 //Receive a value in milliseconds and return an string with
 // the time in the "minutes:seconds" format
 function millisecondsToMinutesAndSeconds(ms: number): string {
-  let minutes = Math.round(ms / 1000 / 60);
-  let seconds = Math.round((ms % 60000) / 1000);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  const date = new Date(ms);
+  return `${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 export { millisecondsToMinutesAndSeconds };

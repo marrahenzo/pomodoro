48c6619aba7e59de2fd93179f02cc73e48fa3017
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-09-28 14:57:36 -0300

    Add start button behavior

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 import Timer from 'tiny-timer';
 import './style.scss';
+import { updateTimer } from './domFunctions';
 
-console.log(new Timer().status);
+//Get time values from form inputs
+const pomodoroInput = document.querySelector(
+  '#time-pomodoro'
+) as HTMLInputElement;
+const restInput = document.querySelector('#time-rest') as HTMLInputElement;
+
+//Convert values to milliseconds
+function getTimeValues() {
+  let pomodoroTime = parseInt(pomodoroInput.value) * 60000;
+  let restTime = parseInt(restInput.value) * 60000;
+  return { pomodoro: pomodoroTime, rest: restTime };
+}
+
+//Get DOM buttons
+const startButton = document.querySelector('#btn-start')!;
+const restartButton = document.querySelector('#btn-restart')!;
+
+//Create new timer and set start button behavior
+const timer = new Timer({ interval: 1000 });
+startButton.addEventListener('click', () => {
+  if (!startButton.classList.contains('started')) {
+    timer.start(getTimeValues().pomodoro);
+    updateTimer(getTimeValues().pomodoro);
+    startButton.classList.add('started');
+  } else if (startButton.classList.contains('paused')) {
+    timer.resume();
+    startButton.classList.toggle('paused');
+  } else {
+    timer.pause();
+    startButton.classList.toggle('paused');
+  }
+});
+
+//Update DOM timer every 1 second
+timer.on('tick', (ms) => {
+  console.log(ms);
+  updateTimer(ms);
+});
